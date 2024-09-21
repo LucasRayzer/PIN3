e4d0ae238d03ad54fea6a6293c44a01067f0ce9c
Lucas Pietro Biasi Rayzer <145388833+LucasRayzer@users.noreply.github.com>
2024-09-21 19:07:04 -0300

    NewViews

--- PIN3/src/pages/adminPages/HomePage/HomePage.styles.jsx
+++ PIN3/src/pages/adminPages/HomePage/HomePage.styles.jsx
@@ -11,19 +11,25 @@ export const HomeBody = styled.div.attrs({
   background-size: cover;
   background-color: #f0f2f5;
 `;
-
-export const UserName = styled.h1.attrs({
-  className: "user-name",
+export const StatusSectionConfig = styled.div.attrs({
+  className: "status-section-config",
 })`
-  font-size: 24px;
-  color: white;
+  display: flex;
+  justify-content: end;
 `;
 
 export const StatusSection = styled.div.attrs({
   className: "status-section",
 })`
   display: flex;
-  gap: 20px;
+  flex-direction: column;
+  gap: 10px;  
+  top: 50px;
+  right: 20px;
+  background-color: #003366;
+  padding: 10px;
+  border-radius: 10px;
+  width: 200px;
 `;
 
 export const StatusItem = styled.div.attrs({
@@ -31,63 +37,82 @@ export const StatusItem = styled.div.attrs({
 })`
   display: flex;
   align-items: center;
-  gap: 8px;
   font-size: 14px;
-  color: ${(props) => props.color || 'white'};
+  color: white;
 `;
 
 export const HomeContainer = styled.div.attrs({
   className: "home-container",
 })`
-  padding: 20px;
+padding:20px;
+  padding-left: 100px;
+  padding-right: 100px;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 export const ProjectsSection = styled.div.attrs({
   className: "projects-section",
 })`
-  flex: 2;
-  h2 {
-    margin-bottom: 10px;
-    font-size: 20px;
-  }
+  display: flex;
+  flex-direction: column;
 `;
 
 export const ReportsSection = styled.div.attrs({
   className: "reports-section",
 })`
-  flex: 1;
-  h2 {
-    margin-bottom: 10px;
-    font-size: 20px;
-  }
+ display: flex;
+ flex-direction: row;
 `;
 
-export const ProjectContainer = styled.div.attrs({
-  className: "project-container",
+export const ScrollContainer = styled.div.attrs({
+  className: "scroll-container",
 })`
   display: flex;
   gap: 10px;
+  overflow-x: auto;
+  padding-bottom: 10px;
+  width: 100%;
+  white-space: nowrap;
+  
+  &::-webkit-scrollbar {
+    height: 10px;
+    background-color:#132979;
+    border-radius: 12px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: white;
+    height: 10px; 
+    border: 2px solid #132979; 
+    border-radius: 10px;
+  }
 `;
 
 export const ProjectCard = styled.div.attrs({
   className: "project-card",
 })`
-  background-color: #003366;
+  background-color: #132979;
   color: white;
   padding: 20px;
   border-radius: 10px;
+  border: 2px solid black;
   position: relative;
-  width: 150px;
-  height: 150px;
+  width: 180px;
+  height: 190px;
+  min-width: 180px; 
+  min-height: 190px; 
+  flex-shrink: 0; 
   display: flex;
+  gap:5px;
   justify-content: center;
   align-items: center;
   text-align: center;
+  flex-direction:column;
   font-size: 14px;
   cursor: pointer;
-
+  
   &::before {
     content: '';
     position: absolute;
@@ -106,27 +131,110 @@ export const ProjectCard = styled.div.attrs({
 export const ReportCard = styled.div.attrs({
   className: "report-card",
 })`
-  background-color: #003366;
+  background-color: #132979;
   color: white;
   padding: 20px;
   border-radius: 10px;
+  border: 2px solid black;
+  position: relative;
+  width: 180px;
+  height: 190px;
+  min-width: 180px; 
+  min-height: 190px; 
+  flex-shrink: 0; 
+  display: flex;
+  gap:5px;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  margin-bottom: 10px;
+  flex-direction:column;
   font-size: 14px;
+  cursor: pointer;
+  
 `;
 
-export const NewReportButton = styled.button.attrs({
-  className: "new-report-button",
-})`
-  background-color: #003366;
-  color: white;
-  padding: 10px;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-  font-size: 14px;
 
-  &:hover {
-    background-color: #001f4d;
-  }
+export const ProjectImage = styled.img.attrs({
+  className: "project-image",
+})`
+  display: flex;
+  height:80px;
+  padding-top:60px;
+`;
+export const ProjectName = styled.h2.attrs({
+  className: "project-name",
+})`
+  display: flex;
+  padding-top:20px;
+  
+`;
+export const ReportImage = styled.img.attrs({
+  className: "relatorio-image",
+})`
+  display: flex;
+  height:80px;
+  padding-top:60px;
+`;
+export const ReportName = styled.h2.attrs({
+  className: "relatorio-name",
+})`
+  display: flex;
+  padding-top:20px;
+  
+`;
+export const Line = styled.div.attrs({
+  className: "line",
+})`
+ background-color:#132979;
+ width: 100%;
+ height: 2px;
+ border-radius:1px;
+ 
+`;
+export const ProjectContainer = styled.div.attrs({
+  className: "project-container",
+})`
+ display:flex;
+ flex-direction:column;
+ gap:10px;
+`;
+export const ReportContainer = styled.div.attrs({
+  className: "relatorio-container",
+})`
+ display:flex;
+ flex-direction:column;
+ gap:10px;
+`;
+export const Title = styled.h1.attrs({
+  className: "title",
+})`
+  color: #000000;
+  font-size: 26px;
+  font-weight: bold;
+  margin:0;
+`;
+export const NewReportImage= styled.img.attrs({
+  className: "new-report-image",
+})`
+  display: flex;
+  height:80px;
+  width:80px;
+  cursor: pointer;
+`;
+export const NewReportContainer = styled.div.attrs({
+  className: "novo-relatorio-container",
+})`
+ display:flex;
+ flex-direction:column;
+ align-items:center;
+ justify-content:center;
+ width:100px;
+ 
+`;
+export const NewReportTitle = styled.h3.attrs({
+  className: "novo-relatorio-title",
+})`
+  display: flex;
+  font-size: 18px;
+  padding-left:10px;
 `;
--- PIN3/src/pages/adminPages/HomePage/HomePage.ui.jsx
+++ PIN3/src/pages/adminPages/HomePage/HomePage.ui.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from "axios";
-import {
-    HomeBody, HomeContainer
-} from './HomePage.styles'; // Estilos conforme seu padrão
 import NavHeader from '../../../components/HeaderMenu/NavHeader.ui';
+import {
+    HomeBody, HomeContainer, StatusSection, StatusItem, ProjectsSection, ReportsSection,
+    ScrollContainer, ProjectCard, ReportCard,
+    StatusSectionConfig,
+    ProjectImage,
+    ProjectName,
+    Line,
+    ProjectContainer,
+    Title,
+    ReportContainer,
+    ReportName,
+    ReportImage,
+    NewReportImage,
+    NewReportContainer,
+    NewReportTitle
+} from './HomePage.styles';
+import ProjectIcon from '../../../assets/images/ProjectIcon.png'; 
+import ReportIcon from '../../../assets/images/RelatorioIcon.png'; 
+import NewReportIcon from '../../../assets/images/NewReport.png'; 
+
 
 export default function HomePage() {
     const navigate = useNavigate();
@@ -14,6 +30,22 @@ export default function HomePage() {
         { name: "Projeto 2", status: "atrasado" },
         { name: "Projeto 3", status: "em andamento" },
         { name: "Projeto 4", status: "em análise" },
+        { name: "Projeto 1", status: "concluido" },
+        { name: "Projeto 2", status: "atrasado" },
+        { name: "Projeto 3", status: "em andamento" },
+        { name: "Projeto 4", status: "em análise" },
+        { name: "Projeto 1", status: "concluido" },
+        { name: "Projeto 2", status: "atrasado" },
+        { name: "Projeto 3", status: "em andamento" },
+        { name: "Projeto 4", status: "em análise" },
+        { name: "Projeto 1", status: "concluido" },
+        { name: "Projeto 2", status: "atrasado" },
+        { name: "Projeto 3", status: "em andamento" },
+        { name: "Projeto 4", status: "em análise" },
+    ];
+
+    const reports = [
+        { name: "Relatório 1" },
     ];
 
     return (
@@ -21,6 +53,60 @@ export default function HomePage() {
             <NavHeader/>
 
             <HomeContainer>
+                <StatusSectionConfig>
+                    <StatusSection>
+                        <StatusItem color="red">
+                            <span>🔴 Projeto atrasado</span>
+                        </StatusItem>
+                        <StatusItem color="yellow">
+                            <span>🟡 Projeto em andamento</span>
+                        </StatusItem>
+                        <StatusItem color="green">
+                            <span>🟢 Projeto concluído</span>
+                        </StatusItem>
+                        <StatusItem color="orange">
+                            <span>🟠 Projeto em análise</span>
+                        </StatusItem>
+                    </StatusSection>
+                </StatusSectionConfig>
+                
+                {/* Projects Section */}
+                <ProjectContainer>
+                    <Title>Meus Projetos</Title>
+                    <Line></Line>
+                <ProjectsSection> 
+                    <ScrollContainer>      
+                        {projects.map((project, index) => (
+                            <ProjectCard key={index} status={project.status}>
+                                   <ProjectImage src={ProjectIcon} alt='Project-Icon'/>
+                                   <ProjectName>{project.name}</ProjectName>
+                            </ProjectCard> 
+                        ))}
+                    </ScrollContainer>
+                </ProjectsSection>
+                </ProjectContainer>
+
+                <ReportContainer>
+                    <Title>Meus Relatórios</Title>
+                    <Line></Line>
+                <ReportsSection>
+                    
+                    <ScrollContainer>
+                        {reports.map((report, index) => (
+                            <ReportCard key={index}>
+                                <ReportImage src={ReportIcon} alt='Project-Icon'/>
+                                 <ReportName>{report.name}</ReportName>
+                                
+                            </ReportCard>
+                        ))}
+                    </ScrollContainer>
+                    <NewReportContainer>
+                                <NewReportImage onClick={() => navigate('/newReport')}
+                                        src={NewReportIcon} alt='New-Report' />
+                                        <NewReportTitle>Novo Relatório</NewReportTitle>
+                        </NewReportContainer>
+                </ReportsSection>
+                </ReportContainer>
                 
             </HomeContainer>
             
